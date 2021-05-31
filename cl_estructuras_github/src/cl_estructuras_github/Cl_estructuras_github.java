522eba8aeeca9215d80fb1a6ce5000f6bbdded06
riosm <riosm@192.168.1.27>
2021-05-31 14:27:36 -0500

    primera versión cl_estructuras_github

---
+++ cl_estructuras_github/src/cl_estructuras_github/Cl_estructuras_github.java
@@ -0,0 +1,11 @@
+
+package cl_estructuras_github;
+
+public class Cl_estructuras_github {
+
+
+    public static void main(String[] args) {
+        System.out.println("taller de conexion netbeans - guthub");
+    }
+    
+}
