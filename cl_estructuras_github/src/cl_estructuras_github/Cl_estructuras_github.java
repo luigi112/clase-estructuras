555e630a333e7af9ccd33b559a522f8c34a33977
luigi112 <85132228+luigi112@users.noreply.github.com>
2021-05-31 14:44:51 -0500

    Update Cl_estructuras_github.java

--- cl_estructuras_github/src/cl_estructuras_github/Cl_estructuras_github.java
+++ cl_estructuras_github/src/cl_estructuras_github/Cl_estructuras_github.java
@@ -6,6 +6,8 @@ public class Cl_estructuras_github {
 
     public static void main(String[] args) {
         System.out.println("taller de conexion netbeans - guthub");
+        system.out.println("version2");
+        
     }
     
 }
